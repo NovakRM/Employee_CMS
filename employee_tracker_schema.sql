3bf177a9f9a6a8f2efac28ef19a7976a5499be54
osteophagy <mnovakahs@gmail.com>
2021-02-21 12:54:03 -0500

    Have tables printing. Logic for adding next.

--- employee_tracker_schema.sql
+++ employee_tracker_schema.sql
@@ -1,35 +1,34 @@
+DROP DATABASE IF EXISTS employee_tracker_db;
 CREATE DATABASE employee_tracker_db;
 
 USE employee_tracker_db;
 
 CREATE TABLE departments (
-    id INT AUTO_INCREMENT,
-    name VARCHAR(30),
-    PRIMARY KEY (id)
+  id INT NOT NULL auto_increment,
+  dept_name VARCHAR(30),
+  PRIMARY KEY (id)
+);
+
+CREATE TABLE roles (
+  id INT NOT NULL auto_increment,
+  title VARCHAR(30),
+  salary DECIMAL,
+  dept_id INT,
+  PRIMARY KEY (id),
+  CONSTRAINT fk_department FOREIGN KEY (dept_id) REFERENCES departments(id)
+);
+
+CREATE TABLE employees (
+  id INT NOT NULL auto_increment,
+  first_name VARCHAR(30),
+  last_name VARCHAR(30),
+  role_id INT,
+  manager_id INT,
+  PRIMARY KEY (id),
+  CONSTRAINT fk_roles FOREIGN KEY (role_id) REFERENCES roles(id),
+  CONSTRAINT fk_manager FOREIGN KEY (role_id) REFERENCES roles(id)
 );
 
 SELECT * FROM departments;
-
-CREATE TABLE roles (
-    id INT AUTO_INCREMENT,
-    title VARCHAR(30),
-    salary DECIMAL(8,2), -- 8 numbers total, 2 after dec, up to 6-fig salary --
-    department_id INT,
-	PRIMARY KEY (id),
-	FOREIGN KEY (department_id) REFERENCES departments(id)
-);
-
 SELECT * FROM roles;
-
-CREATE TABLE employees (
-    id INT AUTO_INCREMENT,
-    first_name VARCHAR(30),
-    last_name VARCHAR(30),
-    role_id INT,
-    manager_id INT NULL,
-    PRIMARY KEY (id),
-    FOREIGN KEY (role_id) REFERENCES roles(id),
-    FOREIGN KEY (manager_id) REFERENCES roles(id)
-);
-
-SELECT * FROM employees;
+SELECT * FROM employees; 
--- index.js
+++ index.js
@@ -1,14 +1,14 @@
 // dependencies
-const mysql = require('mysql')
 const inquirer = require('inquirer')
+const mysql = require('mysql')
 
 // db connection
 const connection = mysql.createConnection({
   host: 'localhost',
   port: 3306,
   user: 'root',
-  password: '****',
-  database: 'employee_tracker_schema',
+  password: 'HellMouthHere@96',
+  database: 'employee_tracker_db',
 })
 
 // on connection:
@@ -21,16 +21,81 @@ connection.connect((err) => {
 const initApp = () =>{
     inquirer.prompt({
         name: 'initialize',
-        type: '',
-        message: '',
-        choices: ['']
+        type: 'list',
+        message: 'What would you like to do?',
+        choices: 
+        [  
+        'View All Employees',
+        'Add Employee',
+        'View All Departments',
+        'Add Department',
+        'View All Roles',
+        'Add Role',
+        'Exit'
+        ]
     })
+
+    //https://gyazo.com/a0764f8197fe385ad6361d70f287aeeb
+    //give this a try at some point? 
+
     .then((res)=>{
         switch(res.initialize)
         {
-        case 'exit':
+        case 'View All Employees':
+            getEmployees()
+        break
+        case 'Add Employee':
+            addEmployee()
+        break
+        case 'View All Departments': 
+            getDepartments()
+        break
+        case 'Add Department':
+            addDepartment()
+        break
+        case 'View All Roles':
+            getRoles()
+        break
+        case 'Add Role':
+            addRole()
+        break
+        case 'Exit':
             connection.end()
+            console.log('Bye!')
         break
         }
     })
 }
+
+const getEmployees = () => {
+    const query = 'SELECT employees.id, employees.first_name, employees.last_name, roles.title, roles.salary, departments.dept_name FROM employees INNER JOIN roles ON employees.role_id=roles.id INNER JOIN departments ON roles.dept_id=departments.id;'
+    connection.query(query, (err, res) => {
+        if (err) throw err
+        console.table(res)
+        initApp()
+    })
+}
+
+const addEmployee = () => {}
+
+const getDepartments = () => {
+    const query = 'SELECT * FROM departments'
+    connection.query(query, (err, res)=>{
+        if (err) throw err
+        console.table(res)
+        initApp()
+    })
+}
+
+const addDepartment = () => {}
+
+const getRoles = () => {
+    const query = 'SELECT title, salary FROM roles INNER JOIN departments ON roles.dept_id = departments.id'
+    connection.query(query, (err, res)=>{
+        if (err) throw err
+        console.table(res)
+        initApp()
+    })
+}
+
+const addRole = () => {}
